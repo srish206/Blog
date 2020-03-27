cc32d6d9c632add4cef0db147c1b3859c94e4959
srish206 <srjain@bestpeers.com>
2020-03-27 14:36:14 +0530

    updated code

--- Wordoid/forms.py
+++ Wordoid/forms.py
@@ -1,31 +1,28 @@
-
 from django import forms
 from django.forms import ModelForm
-from Wordoid.models import Post
-from Wordoid.models import Comment
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
+from Wordoid.models import Post
+from Wordoid.models import Comment
+
 
 class PostForm(forms.ModelForm):
-
-	''' this init method created to get a request user in a model form for this method we can 
-		directly access user field withod mention into a Meta class fields (user)'''	
-	
-	def __init__(self,request, *args, **kwargs):
+	''' this method is use to get a request of user in a model form '''
+	def __init__(self, request, *args, **kwargs):
 		self.request = request
 		super(PostForm, self).__init__(*args, **kwargs)
 
 	class Meta:
 		model = Post
 		# fields = '__all__'
-		fields = ('title','description','publish')
+		fields = ('title', 'description', 'publish')
+
 
 class CommentForm(forms.ModelForm):
-	
 	class Meta:
 		model = Comment
 		# fields = ('post','text_field',)
-		fields = ('text_field',)
+		fields = ('text_field', )
 		# labels = {
 		# 	'text_field': ('write comment here'),
 		# }
